1f6f207f46a2be08a9d325a639a8926cbd5a4348
retr0lbb <retr0lbb@gmail.com>
2026-09-23 22:38:35 -0300

    installed native wind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,12 +3,13 @@ import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "./global.css";
 
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
+  ErrorBoundary
 } from 'expo-router';
 
 export const unstable_settings = {
